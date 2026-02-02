edaa4f8e7b265ea4997f90023e51e1487e214c96
simonshelke44 <shelkesimonofficial1@gmail.com>
2026-02-02 11:15:30 +0530

    even nos pg

---
+++ homeworkprob3.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class homeworkprob3 {
+    public static void main(String[] args){
+        //h1 
+        /*Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the even numbers limit you want: ");
+        int n = sc.nextInt();
+
+        for (int i =1; i<= n ; i++){
+            if(i%2 == 0){
+                System.out.println(i);
+            }else{
+                continue; // here we are 
+            }
+        }*/
+
+        //h2
+        
+        for(; ;) {
+            System.out.println("Apna College");
+        }
+
+    }
+}
