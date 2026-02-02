93ce479bad90553a5d9cb1aaac18f5a4e1f18eb0
simonshelke44 <shelkesimonofficial1@gmail.com>
2026-02-02 12:56:09 +0530

    2nd pg on odd even

--- evenodd.java
+++ evenodd.java
@@ -1,6 +1,10 @@
 // Check whether a number is even or odd
 
-import java.util.*;
+
+//Qs. Print if a number is prime or not (Input n from the user). 
+//[In this problem you will learn how to check if a number is prime or not]
+
+/*import java.util.*;
 public class evenodd {
     public static void main(String[] args) {
         System.out.println("Enter the integer value: ");
@@ -12,5 +16,26 @@ public class evenodd {
         }else{
             System.out.println("The number " + n + " is odd.");
         }
+    }
+}*/
+
+
+import java.util.*;
+
+public class evenodd{
+    public static void main(String[] args) {
+        System.out.println("Enter the integer you want to check if even or odd: ");
+
+        Scanner sc =  new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if(n%2 ==0){
+            System.out.println("The number " + n + " is even.");
+        }else if(n%2 ==1){
+            System.out.println("The number " + n + " is odd.");
+        }else{
+            System.out.println("Invalid input.");
+        }
+        
     }
 }
